26b2b8621513807fbecd3b04b291eab085edc218
prayogaekaardiansyah <prayogaekaardiansyah@gmail.com>
2022-05-17 10:55:48 +0700

    add: ArrayMultidimensi.java

--- Basic/ArrayMultidimensi.java
+++ Basic/ArrayMultidimensi.java
@@ -11,8 +11,20 @@ public class ArrayMultidimensi {
         String toyotaSport = cars[0][1];
         System.out.println(toyotaSport);
 
+        for (int i = 0; i < cars.length; i++) {
+            for (int j = 0; j < cars[i].length; j++) {
+                System.out.println(cars[i][j]);
+            }
+        }
+
         int[][] number = {{1,2,3,4},{5,6,7,8,9,0}};
         int seven = number[1][2];
         System.out.println(seven);
+
+        for (int i = 0; i < number.length; i++) {
+            for (int j = 0; j < number[i].length; j++) {
+                System.out.println(number[i][j]);
+            }
+        }
     }
 }
