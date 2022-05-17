5eb50e80875a3fd588f37186c1dfa79e43e358bf
prayogaekaardiansyah <prayogaekaardiansyah@gmail.com>
2022-05-17 09:10:58 +0700

    add: Booleans.java

---
+++ Basic/Booleans.java
@@ -0,0 +1,16 @@
+package Basic;
+
+/**
+ * Booleans
+ */
+public class Booleans {
+
+    public static void main(String[] args) {
+        
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(benar);
+        System.out.println(salah);
+    }
+}
