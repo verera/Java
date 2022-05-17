38c35970294e0e229824b910e75b8cb94e445336
prayogaekaardiansyah <prayogaekaardiansyah@gmail.com>
2022-05-17 09:37:07 +0700

    add: WhileLoop.java

---
+++ Basic/WhileLoop.java
@@ -0,0 +1,17 @@
+package Basic;
+
+/**
+ * WhileLoop
+ */
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        
+        int counter = 0;
+
+        while(counter <= 10){
+            System.out.println("Counter -> " + counter);
+            counter++;
+        }
+    }
+}
