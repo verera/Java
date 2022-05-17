a8ce3ce774f207fcb00f4bfbd373fb959af2e287
prayogaekaardiansyah <prayogaekaardiansyah@gmail.com>
2022-05-17 08:22:30 +0700

    add: Operator.java

---
+++ Basic/Operator.java
@@ -0,0 +1,54 @@
+package Basic;
+
+/**
+ * Operator
+ */
+public class Operator {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Operator Aritmatika
+         * 
+         * Addition         [ + ]
+         * Subtraction      [ - ]
+         * Multiplication   [ * ]
+         * Division         [ / ]
+         * Modulus          [ % ]
+         * Increment        [ ++ ]
+         * Decrement        [ -- ]
+         */
+        
+
+        /**
+         * Operator Penugasan
+         * 
+         *  =   [x = 5] [x = 5]
+         *  +=   [x += 5] [x += 5] 
+         *  -=   [x -= 5] [x -= 5] 
+         *  *=   [x *= 5] [x *= 5] 
+         *  /=   [x /= 5] [x /= 5] 
+         *  %=   [x %= 5] [x %= 5]
+         */
+
+        /**
+         * Operator Perbandingan
+         * 
+         * ==   [x == y]
+         * ===   [x === y]
+         * !=   [x != y]
+         *  >   [x > y]
+         *  <   [x < y]
+         * >=   [x >= y]
+         * <=   [x <= y]
+         */
+
+        /**
+         * Operator Logika
+         * 
+         * &&   [x > 5 && x < 10]
+         * ||   [x > 5 || x < 10]
+         * !   !(x < 5 && x < 10)
+         */
+    }
+}
