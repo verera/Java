a53ac39bf5835838859faf692713c88128844248
prayogaekaardiansyah <prayogaekaardiansyah@gmail.com>
2022-05-17 09:40:18 +0700

    add:FoorLoop.java

---
+++ Basic/FoorLoop.java
@@ -0,0 +1,15 @@
+package Basic;
+
+/**
+ * FoorLoop
+ */
+public class FoorLoop {
+
+    public static void main(String[] args) {
+        
+        for (int counter = 0; counter < 10; counter++) {
+            
+            System.out.println("Counter -> " + counter);
+        }
+    }
+}
