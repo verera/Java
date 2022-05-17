08b372b678acd710903b92d10463d2a4e87dd290
prayogaekaardiansyah <prayogaekaardiansyah@gmail.com>
2022-05-17 08:40:52 +0700

    add: SpecialCharacters.java

---
+++ Basic/SpecialCharacters.java
@@ -0,0 +1,23 @@
+package Basic;
+
+/**
+ * SpecialCharacters
+ */
+public class SpecialCharacters {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Escape Character
+         * 
+         * [ \' ]   single quote
+         * [ \" ]   double quote
+         * [ \\ ]   Backslash
+         * [ \n ]   new line
+         * [ \r ]   carriage return
+         * [ \t ]   tab
+         * [ \b ]   backspace
+         * [ \f ]   form feed
+         */
+    }
+}
