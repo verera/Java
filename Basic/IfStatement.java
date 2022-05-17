d0fb18859dbc5beb278f20db244bb689d42ae721
prayogaekaardiansyah <prayogaekaardiansyah@gmail.com>
2022-05-17 09:27:53 +0700

    add: IfStatement.java

--- Basic/IfStatement.java
+++ Basic/IfStatement.java
@@ -7,14 +7,21 @@ public class IfStatement {
 
     public static void main(String[] args) {
         
-        int time = 12;
+        int time = 20;
 
-        if(time < 10){
+        if(time < 12){
             System.out.println("Good Morning");
-        }else if(time < 20){
+        }else if(time < 18){
             System.out.println("Good Afternoon");
         }else{
             System.out.println("Good Night");
         }
+
+        /**
+         * Operator Ternary
+         */
+        int times = 20;
+        String day = (times < 12) ? "Good day" : "Good Evening";
+        System.out.println(day);
     }
 }
