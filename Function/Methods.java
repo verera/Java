4a864ca98217ed9d24e034ebc2623f2457ff3b7b
prayogaekaardiansyah <prayogaekaardiansyah@gmail.com>
2022-05-18 08:46:15 +0700

    add: Methods.java

---
+++ Function/Methods.java
@@ -0,0 +1,14 @@
+package Function;
+
+public class Methods {
+    
+    public static void main(String[] args) {
+        
+        sayHello();
+    }
+
+    static void sayHello() {
+
+        System.out.println("Hello, Methods!");
+    }
+}
